50a4ef4c398fef5b1e61c7f1fd8e995bdda8eefe
JeetDholu <jeetdholu112@gmail.com>
2026-05-17 12:56:51 +0530

    render fix

--- app.js
+++ app.js
@@ -51,7 +51,9 @@ app.get("/branches", (req, res) => {
     res.render("branches");
 });
 
-    app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
         console.log("Server started on port 3000");
     });
 
